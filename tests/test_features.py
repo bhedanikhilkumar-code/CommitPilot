7caa57f0cbba9e1126343bb4fbbafa669c08b59c
bhedanikhilkumar-code <226649017+bhedanikhilkumar-code@users.noreply.github.com>
2026-02-28 15:02:40 +0530

    test: add validation test case 14

--- tests/test_features.py
+++ tests/test_features.py
@@ -4,3 +4,7 @@ from src.features.validators import *
 def test_validate_goal_6():
     assert validate_goal_6(100)
     assert not validate_goal_6(0)
+
+def test_validate_goal_14():
+    assert validate_goal_14(100)
+    assert not validate_goal_14(0)
