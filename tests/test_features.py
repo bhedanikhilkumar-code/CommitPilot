5a3b06e139b259ac70e7af1b45cdef34d532764f
bhedanikhilkumar-code <226649017+bhedanikhilkumar-code@users.noreply.github.com>
2026-02-28 15:05:28 +0530

    test: add task title validator test 355

--- tests/test_features.py
+++ tests/test_features.py
@@ -184,3 +184,9 @@ def test_validate_task_title_345():
     from src.features.validators import validate_task_title_345
     assert validate_task_title_345('Ship feature')
     assert not validate_task_title_345('x')
+
+
+def test_validate_task_title_355():
+    from src.features.validators import validate_task_title_355
+    assert validate_task_title_355('Ship feature')
+    assert not validate_task_title_355('x')
