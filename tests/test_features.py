17419c058572a0c1827f40e4f9c4813afaec0528
bhedanikhilkumar-code <226649017+bhedanikhilkumar-code@users.noreply.github.com>
2026-02-28 16:52:02 +0530

    test: add checkpoint id validator test 3125

--- tests/test_features.py
+++ tests/test_features.py
@@ -1694,3 +1694,9 @@ def test_validate_checkpoint_id_3115():
     from src.features.validators import validate_checkpoint_id_3115
     assert validate_checkpoint_id_3115(1)
     assert not validate_checkpoint_id_3115(0)
+
+
+def test_validate_checkpoint_id_3125():
+    from src.features.validators import validate_checkpoint_id_3125
+    assert validate_checkpoint_id_3125(1)
+    assert not validate_checkpoint_id_3125(0)
