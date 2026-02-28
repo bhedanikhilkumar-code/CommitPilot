183dd6eab49510c0bbac306f13b3010a85beea0e
bhedanikhilkumar-code <226649017+bhedanikhilkumar-code@users.noreply.github.com>
2026-02-28 15:03:10 +0530

    test: add validation test case 214

--- tests/test_features.py
+++ tests/test_features.py
@@ -104,3 +104,7 @@ def test_validate_goal_198():
 def test_validate_goal_206():
     assert validate_goal_206(100)
     assert not validate_goal_206(0)
+
+def test_validate_goal_214():
+    assert validate_goal_214(100)
+    assert not validate_goal_214(0)
