cf7f04af3be24524549ea5f5d35c90b38508900f
bhedanikhilkumar-code <226649017+bhedanikhilkumar-code@users.noreply.github.com>
2026-02-28 15:49:31 +0530

    test: add commit note validator test 1815

--- tests/test_features.py
+++ tests/test_features.py
@@ -908,3 +908,9 @@ def test_validate_commit_note_1805():
     from src.features.validators import validate_commit_note_1805
     assert validate_commit_note_1805('use clear message')
     assert not validate_commit_note_1805('no')
+
+
+def test_validate_commit_note_1815():
+    from src.features.validators import validate_commit_note_1815
+    assert validate_commit_note_1815('use clear message')
+    assert not validate_commit_note_1815('no')
