f6432d4b13ded59fdc4e3f56977b943f5d077110
bhedanikhilkumar-code <226649017+bhedanikhilkumar-code@users.noreply.github.com>
2026-02-28 16:57:54 +0530

    feat(planner): add priority bucket 4812

--- src/features/planner.py
+++ src/features/planner.py
@@ -2540,3 +2540,9 @@ def priority_bucket_4802(limit: int = 5):
     s = load_state()
     tasks = s.get('tasks', [])
     return [x.get('title', 'Untitled') for x in tasks[:limit]]
+
+
+def priority_bucket_4812(limit: int = 5):
+    s = load_state()
+    tasks = s.get('tasks', [])
+    return [x.get('title', 'Untitled') for x in tasks[:limit]]
